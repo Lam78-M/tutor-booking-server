cb3d4bed251633aff741d35a576afbf1b321c043
Lam272 <ayatlam787@gmail.com>
2026-06-03 22:56:31 +0600

    booked session done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI
-
+   
 
 // name:mediqueuebookstore
 // password: T9kuS5vBBa1UH2IAno
@@ -32,13 +32,90 @@ async function run() {
     const db = client.db('mediqueue')
     const addTutorCollection = db.collection('addtutors')
     const addTutorAllCollection = db.collection('tutorall')
+    const bookingCollection = db.collection("bookings")
+
+    //booking settings
+app.post("/bookings", async (req, res) => {
+  try {
+    const booking = req.body;
+
+    const result = await bookingCollection.insertOne(booking);
+
+    res.json({
+      success: true,
+      insertedId: result.insertedId,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+});
     
+
+//book get method
+app.get("/bookings", async (req, res) => {
+  try {
+    const result = await bookingCollection.find().toArray();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+});
 //tyt dto dkiakdefjie iofehf
 
-app.get('/tutor', async (req,res)=>{
- const result = await addTutorAllCollection.find().toArray();
- res.send(result)
-})
+app.get('/tutor', async (req, res) => {
+  const { search = "", startDate, endDate } = req.query;
+
+  let query = {};
+
+  // Name Search (case insensitive)
+  if (search) {
+    query.tutorName = {
+      $regex: search,
+      $options: "i",
+    };
+  }
+
+  // Date Filter
+  if (startDate && endDate) {
+    query.sessionStart = {
+      $gte: startDate,
+      $lte: endDate,
+    };
+  }
+
+  const result = await addTutorAllCollection.find(query).toArray();
+
+  res.send(result);
+});
+
+
+// 00000000000000000000000000000000000000000000
+
+// 00000000000000000000000000000000000
+
+app.get("/tutor/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid ID" });
+    }
+
+    const tutor = await addTutorAllCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    if (!tutor) {
+      return res.status(404).json({ message: "Tutor not found" });
+    }
+
+    res.json(tutor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+});
+
 
   //  -------------------------------
    //get data  to the front end
@@ -81,6 +158,8 @@ app.get('/tutor', async (req,res)=>{
      const result = await  addTutorCollection.deleteOne({_id: new ObjectId(id)})
      res.json(result)
    })
+
+   
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
